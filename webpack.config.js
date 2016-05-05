142d7feb39e5836ff81c310541e3188716867b1e
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-05 16:17:50 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path:__dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}

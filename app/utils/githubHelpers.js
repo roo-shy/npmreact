a6e37c532ae197f03f8711198d50df9610daf829
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-16 00:33:46 -0500

    Github Helpers File Edit

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,15 +8,35 @@ function getUserInfo (username) {
 }
 
 function getRepos (username) {
-  //fetch username repos
+   return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
 }
 
-function getTotalStars (stars) {
-  //calculate all the stars that the user has
-}
+function getTotalStars (repos) {
+   return repos.data.reduce(function (prev, current) {
+     return prev + current.stargazers_count
+   }, 0)
+ }
 
 function getPlayersData (player) {
-  //
+  // get Repos
+  // getTotalStars
+  // return object with that data
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+  }
+
+function calculateScores (players) {
+  //return an array, after doing some algorithms to define a winner
+   return [
+     players[0].followers * 3 + players[0].totalStars,
+     players[1].followers * 3 + players[1].totalStars
+   ]
 }
 
 var helpers = {
@@ -33,6 +53,12 @@ var helpers = {
     })
   },
   battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
 
   }
 };

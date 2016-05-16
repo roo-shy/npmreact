abc583df3479024d91cea3146700ae51107fa17a
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-15 22:46:13 -0500

    New Component User Details Wrapper

---
+++ app/components/UserDetailsWrapper.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function UserDetailWrapper (props) {
+  return (
+    <div className='col-sm-6'>
+     <p className='lead'>Player 1</p>
+     </div>
+  )
+}
+
+module.exports =UserDetailWrapper;

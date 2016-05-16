4792f77811683e2ad535e9d954c1c846f85071f4
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-15 23:28:39 -0500

    New Component Results Container

---
+++ app/containers/ResultsContainer.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var Results = require('..components/Results');
+
+function ResultsContainer (props) {
+  return (
+     <Results />
+  )
+}
+
+module.exports = ResultsContainer;

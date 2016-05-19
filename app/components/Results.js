5b3a1e422dfa653b8739c6ab6f936a6fe1e80169
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-18 22:06:21 -0500

    UI for Results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,25 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function puke (obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function Results (props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div>Results: {puke(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    <h1></h1>
+    <div className='col-sm-8 col-sm-offset-2'>
+      <UserDetailsWrapper header='Winner'>
+        <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}/>
+      </UserDetailsWrapper>
+      <UserDetailsWrapper header='Loser'>
+         <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}/>
+      </UserDetailsWrapper>
+
+    </div>
+    </div>
   )
 }
 
